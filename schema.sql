37b038df18b6602560045778eaa3801c8db9560c
Lance Brignoni <lance@brignoni.space>
2016-02-14 20:09:10 -0800

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE files (     
+  id INT NOT NULL AUTO_INCREMENT,     
+  name VARCHAR(30),     
+  description TEXT,     
+  type VARCHAR(30),     
+  size INT,     
+  path VARCHAR(90),     
+  PRIMARY KEY(id) );
