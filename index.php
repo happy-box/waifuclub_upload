3759d905e81c4c606b467a994b48439fde8151e2
Lance Brignoni <lance@brignoni.space>
2016-02-14 20:03:53 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+	<h1> Upload some shit fam </h1>
+	<form action="upload.php" method="POST" enctype="multipart/form-data">
+		<input type="file" name="myfile"><p>
+		<input type="submit" value="Upload">
+	</form>
+
+</html>
